0263ee5752b1a707d0d132c98bb7f11c561efb08
Fts99 <47905996+Fts99@users.noreply.github.com>
2020-10-30 01:26:03 -0300

    Update AuthorizationServerConfig.java

--- src/main/java/com/onlycoders/backendalugo/configuracao/AuthorizationServerConfig.java
+++ src/main/java/com/onlycoders/backendalugo/configuracao/AuthorizationServerConfig.java
@@ -48,6 +48,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret("aluGo@123!321")
                 .scopes("read", "write")
                 .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds((60 * 60) * 24);
+                .accessTokenValiditySeconds((6000 * 600) * 24);
     }
 }
