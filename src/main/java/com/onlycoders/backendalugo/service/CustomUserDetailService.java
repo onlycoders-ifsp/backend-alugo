d1ea4b2e3990d65c3a27d87908246698278c081b
Fts99 <47905996+Fts99@users.noreply.github.com>
2020-10-29 23:56:04 -0300

    Update CustomUserDetailService.java

--- src/main/java/com/onlycoders/backendalugo/service/CustomUserDetailService.java
+++ src/main/java/com/onlycoders/backendalugo/service/CustomUserDetailService.java
@@ -27,7 +27,8 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        RetornaLogin retornaLogin = repository.verificaLogin(login.toLowerCase());
+        //login = login.toLowerCase();
+        RetornaLogin retornaLogin = repository.verificaLogin(login);
 
         UsuarioLogin user = new UsuarioLogin();
 
