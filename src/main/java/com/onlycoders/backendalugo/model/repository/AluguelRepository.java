f65a4512e3a296dd17b1eb4eb88abc989af46f49
Fts99 <47905996+Fts99@users.noreply.github.com>
2021-02-04 04:21:01 -0300

    ajuste

--- src/main/java/com/onlycoders/backendalugo/model/repository/AluguelRepository.java
+++ src/main/java/com/onlycoders/backendalugo/model/repository/AluguelRepository.java
@@ -153,7 +153,7 @@ public interface AluguelRepository extends JpaRepository<Produto, Integer> {
 
     @Transactional(isolation = Isolation.READ_UNCOMMITTED)
     @Query(value = "SELECT *FROM FN_RETORNA_CHK_DEVOLUCAO(:id_aluguel,:usuario)" +
-            "AS (IDALUGUEL TEXT, DESCRICAO TEXT," +
+            "AS (ID_ALUGUEL TEXT, DESCRICAO TEXT," +
             "FOTO_PRODUTO BYTEA, OK_LOCADOR BOOLEAN);",nativeQuery = true)
     RetornaChecklist retornaCheckListDevolucao(@Param("id_aluguel") String id_aluguel,
                                                @Param("usuario") String usuario);
