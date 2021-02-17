27626c854c4a8904e9d0b9e512d725ab3716b61c
Fts99 <47905996+Fts99@users.noreply.github.com>
2021-02-17 20:55:53 -0300

    Update ResourceServerConfig.java

--- src/main/java/com/onlycoders/backendalugo/configuracao/ResourceServerConfig.java
+++ src/main/java/com/onlycoders/backendalugo/configuracao/ResourceServerConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+
+import java.time.Duration;
 
 @Configuration
 @EnableResourceServer
@@ -21,6 +24,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/aluguel/notificacoes").permitAll()
                 .antMatchers("/aluguel/**").hasAuthority("ROLE_USER")
                 .antMatchers("/pagamento/retorno-pagamento").permitAll()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+        .and()
+                .headers()
+                .cacheControl().disable()
+                .contentTypeOptions().disable()
+                .httpStrictTransportSecurity().includeSubDomains(true).maxAgeInSeconds(Duration.ofDays(365).toMinutes()/60)
+                .and()
+                .frameOptions().sameOrigin()
+                .xssProtection().disable()
+                .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN)
+                .and().contentSecurityPolicy("script-src 'self' https://api.mercadopago.com https://viacep.com.br");
     }
 }
